4b1f0dca8a7c8fbf45a234e546bed7b2c15467f1
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2016-12-03 17:16:53 +0900

    Add next.js

---
+++ components/head.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default () => (
+  <Head>
+    <title>Hello Next</title>
+    <link rel="stylesheet" href="/static/index.css"/>
+  </Head>
+)
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import css from 'next/css'
+import Head from '../components/head'
+
+export default () => <div>
+  <Head/>
+  <h1>Hello world!</h1>
+</div>
