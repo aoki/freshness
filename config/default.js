c0125d5585c31270b13a636b5b7dc3b01da342ae
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2016-12-03 19:32:47 +0900

    Add config

---
+++ config/default.js
@@ -0,0 +1,8 @@
+export default {
+  github:{
+    scheme: 'https',
+    domain: 'api.github.com',
+    apiEndPoint: '',
+    organizations: ['nodejs', 'zeit']
+  }
+}
--- pages/about.js
+++ pages/about.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Link from 'next/link';
 import pk from '../package.json'
 import Header from '../components/header'
+import config from '../config/default'
 
 export default () => <div>
   <Header title="About pr"/>
   <Link href="/index">Index</Link>
   <pre>{JSON.stringify(pk, null, ' ')}</pre>
+  <pre>{JSON.stringify(config, null, ' ')}</pre>
 </div>
