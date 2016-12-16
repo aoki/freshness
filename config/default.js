36ee2113b2e53d1d343654e4957c9c1e5370716e
aoki <ringohub@gmail.com>
2016-12-16 21:13:27 +0900

    Update default.js

--- config/default.js
+++ config/default.js
@@ -13,5 +13,8 @@ export default {
 export const GITHUB_API_URL = 'https://api.github.com';
 export const reposUrl = (orgName) => { return `${GITHUB_API_URL}/orgs/${orgName}/repos?per_page=100`; }
 export const prsUrl = (orgName, repoName) => { return `${GITHUB_API_URL}/repos/${orgName}/${repoName}/pulls?per_page=100`; };
+export const orgUrl = orgName => {
+  return `${GITHUB_API_URL}/orgs/${orgName}`;
+};
 export const GITHUB_TOKEN = '' // process.env.PR_GITHUB_TOKEN
 export const config = { headers: {Authorization: `token ${GITHUB_TOKEN}`} };
