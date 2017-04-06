6480c2760add128b51415a1097f0b376f340f903
Yoshiki Aoki <ringohub@gmail.com>
2017-04-06 21:56:54 +0900

    Fix #15: Inject api token from environment variables

--- config/default.js
+++ config/default.js
@@ -1,20 +1,22 @@
+/*
+global GITHUB_API_TOKEN
+global GITHUB_API_URL
+global TARGET_ORG_LIST
+*/
+
 export default {
   github:{
-    scheme: 'https',
-    domain: 'api.github.com',
-    apiEndPoint: '',
     target: {
       // users: ['ringohub'],
-      organizations: ['dwango', 'dwango-js', 'opentoonz', 'asciidwango', 'nico-opendata']
+      organizations: TARGET_ORG_LIST.replace(/\s/g,'').split(',')
     }
   }
 }
 
-export const GITHUB_API_URL = 'https://api.github.com';
 export const reposUrl = (orgName) => { return `${GITHUB_API_URL}/orgs/${orgName}/repos?per_page=100`; }
 export const prsUrl = (orgName, repoName) => { return `${GITHUB_API_URL}/repos/${orgName}/${repoName}/pulls?per_page=100`; };
 export const orgUrl = orgName => {
   return `${GITHUB_API_URL}/orgs/${orgName}`;
 };
-export const GITHUB_TOKEN = '' // process.env.PR_GITHUB_TOKEN
+export const GITHUB_TOKEN = GITHUB_API_TOKEN;
 export const config = { headers: {Authorization: `token ${GITHUB_TOKEN}`} };
---
+++ env-config.js
@@ -0,0 +1,10 @@
+const prod = process.env.NODE_ENV === 'production'
+const githubApiUrl    = process.env.FRESHNESS_GITHUB_API_URL    || 'https://api.github.com'
+const githubApiToken  = process.env.FRESHNESS_GITHUB_API_TOKEN
+const targetOrgs      = process.env.FRESHNESS_TARGET_ORGS       || 'github,nodejs'
+
+module.exports = {
+  'GITHUB_API_URL': githubApiUrl,
+  'GITHUB_API_TOKEN': githubApiToken,
+  'TARGET_ORG_LIST': targetOrgs
+}
