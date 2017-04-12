aed54fca96ce1e99828e1c1be208f4ed91750a23
Yoshiki Aoki <ringohub@gmail.com>
2017-04-12 21:33:11 +0900

    Add progress bar

--- components/header.js
+++ components/header.js
@@ -1,5 +1,14 @@
 import React, {Component, PropTypes} from 'react';
 import Head from 'next/head';
+import NProgress from 'nprogress'
+import Router from 'next/router'
+
+Router.onRouteChangeStart = (url) => {
+  console.log(`Loading: ${url}`)
+  NProgress.start()
+}
+Router.onRouteChangeComplete = () => NProgress.done()
+Router.onRouteChangeError = () => NProgress.done()
 
 class Header extends Component {
 
@@ -15,6 +24,7 @@ class Header extends Component {
         <link rel="icon shortcut" href="/static/favicon.icon" type="image/x-icon"/>
         <link rel="icon" sizes="any" href="/static/favicon.svg" type="image/svg+xml"/>
         <link rel="mask-icon" href="/static/favicon.svg"/>
+        <link rel='stylesheet' type='text/css' href='/static/nprogress.css' />
       </Head>
     );
   }
